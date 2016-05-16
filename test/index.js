27142ec008061cbc469ba25ee5e111ad2d5e9cde
MaxMEllon <maxmellon1994@gmail.com>
2016-05-17 02:12:54 +0900

    close #1 Fix: this.someVar.isNil not working.

--- test/index.js
+++ test/index.js
@@ -19,6 +19,16 @@ const specs = [
     after: '"use strict";\n\nhoge === null || hoge === undefined;'
   },
   {
+    description: 'expect ! isNil replace to `! (=== null || === undefined)`',
+    before: '!hoge.isNil',
+    after: '"use strict";\n\n!(hoge === null || hoge === undefined);'
+  },
+  {
+    description: 'expect isNil replace to `=== null || === undefined`',
+    before: 'class Hoge { constructor(hoge) { if (this.hoge.isNil) { this.hoge = hoge; } } }',
+    after: '"use strict";\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }\n\nvar Hoge = function Hoge(hoge) {\n  _classCallCheck(this, Hoge);\n\n  if (this.hoge === null || this.hoge === undefined) {\n    this.hoge = hoge;\n  }\n};'
+  },
+  {
     description: 'expect isNil() dont replace',
     before: 'R.isNil()',
     after: '"use strict";\n\nR.isNil();'
